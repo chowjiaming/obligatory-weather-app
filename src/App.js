3e31947fdadf44d5d74e94b2539b4cc08ac6281f
Joefs <joefstack@gmail.com>
2021-10-29 16:56:39 -0400

    setup basic file structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Main from "./components/Main/Main.js";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
 }
 
 export default App;
